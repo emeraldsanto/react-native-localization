0ba50b8f54e09e29f31561092a189930c7adb49c
emeraldsanto <yanick.belanger@yahoo.com>
2020-07-29 00:40:48 -0400

    Remove whitespace

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@
 export * from "./components/LocalizedText/LocalizedText";
 export * from "./contexts/LocalizationContext/LocalizationContext";
 export * from "./contexts/LocalizationContext/LocalizationProvider";
-
